1f90c83a3e999ef8f5dc7ebc9e15069bc74af2ed
Valeriy Zadera <vszadera@gmail.com>
2024-04-11 00:34:10 +0500

    Верстка

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Container = ({
+	className = "block"
+}) => {
+	return (
+		<div className={className}>
+			<div className="scroll"></div>
+		</div>
+	);
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<Container 
+				className = "user_info block"
+			/>
+			<Container 
+				className = "user_skills block"
+			/>
+			<Container 
+				className = "user_technology block"
+			/>
+		</>
+	)
 }
 
 export default App
